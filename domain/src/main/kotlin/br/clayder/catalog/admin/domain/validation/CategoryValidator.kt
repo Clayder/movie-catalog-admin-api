3e7553dd2bfe8a5773158a0e7b869ff000d46e2c
Clayder <peterclayder@gmail.com>
2024-01-23 21:34:58 -0300

    feat: 3.10 Implement name validation

--- domain/src/main/kotlin/br/clayder/catalog/admin/domain/validation/CategoryValidator.kt
+++ domain/src/main/kotlin/br/clayder/catalog/admin/domain/validation/CategoryValidator.kt
@@ -8,8 +8,20 @@ class CategoryValidator(
 ): Validator(handler) {
 
     override fun validate() {
-        if (category.aName.isEmpty()) {
+        checkNameConstraints()
+    }
+
+    private fun checkNameConstraints() {
+        val name = category.aName
+        if (name.isEmpty()) {
             validationHandler().append(Error("'name' should not be empty"))
+        }
+        if (name.isBlank()) {
+            validationHandler().append(Error("'name' should not be blank"))
+        }
+        val length = name.trim().length
+        if (length > 255 || length < 3) {
+            validationHandler().append(Error("'name' must be between 3 and 255 characters"))
         }
     }
 }
--- domain/src/test/kotlin/br/clayder/catalog/admin/domain/category/CategoryTest.kt
+++ domain/src/test/kotlin/br/clayder/catalog/admin/domain/category/CategoryTest.kt
@@ -5,6 +5,7 @@ import br.clayder.catalog.admin.domain.validation.handler.ThrowsValidationHandle
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class CategoryTest {
 
@@ -20,24 +21,25 @@ class CategoryTest {
             expectedIsActive
         )
 
+        assertDoesNotThrow { actualCategory.validate(ThrowsValidationHandler()) }
+
         Assertions.assertNotNull(actualCategory)
         Assertions.assertNotNull(actualCategory.id)
         Assertions.assertEquals(expectedName, actualCategory.aName)
         Assertions.assertEquals(expectedDescription, actualCategory.aDescription)
-        Assertions.assertEquals(expectedIsActive, actualCategory.isActive)
+        Assertions.assertTrue(actualCategory.isActive)
         Assertions.assertNotNull(actualCategory.aCreatedAt)
         Assertions.assertNotNull(actualCategory.aUpdatedAt)
         Assertions.assertNull(actualCategory.aDeletedAt)
     }
 
     @Test
-    fun `Given an invalid params when call new category then instantiate a category`() {
+    fun `Given an invalid empty name when call new category and validate then should receive error`() {
         val expectedName = ""
         val expectedErrorCount = 1
         val expectedErrorMessage = "'name' should not be empty"
         val expectedDescription = "A categoria mais assistida"
         val expectedIsActive = true
-
 
         val actualCategory = Category.newCategory(
             expectedName,
@@ -55,5 +57,107 @@ class CategoryTest {
 
         Assertions.assertEquals(expectedErrorCount, actualException.errors.size)
         Assertions.assertEquals(expectedErrorMessage, actualException.errors[0].message)
+    }
+
+    @Test
+    fun `Given an invalid name length than 3 when call new category and validate then should receive error`() {
+        val expectedName = "Fi "
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "'name' must be between 3 and 255 characters"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val actualCategory = Category.newCategory(
+            expectedName,
+            expectedDescription,
+            expectedIsActive
+        )
+
+        val actualException = assertThrows(
+            DomainException::class.java
+        ) {
+            actualCategory.validate(
+                ThrowsValidationHandler()
+            )
+        }
+
+        Assertions.assertEquals(expectedErrorCount, actualException.errors.size)
+        Assertions.assertEquals(expectedErrorMessage, actualException.errors[0].message)
+    }
+
+    @Test
+    fun `Given an invalid name length more then 255 characters when call new category and validate then should receive error`() {
+        val expectedName = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies feugiat sapien, sed vestibulum " +
+                "justo pharetra eget. Duis ut lectus fermentum, dictum ante sit amet, ultricies ex. Duis venenatis eget dui ut aliquam." +
+                " Donec porttitor sapien leo. Sed ac orci risus"
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "'name' must be between 3 and 255 characters"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val actualCategory = Category.newCategory(
+            expectedName,
+            expectedDescription,
+            expectedIsActive
+        )
+
+        val actualException = assertThrows(
+            DomainException::class.java
+        ) {
+            actualCategory.validate(
+                ThrowsValidationHandler()
+            )
+        }
+
+        Assertions.assertEquals(expectedErrorCount, actualException.errors.size)
+        Assertions.assertEquals(expectedErrorMessage, actualException.errors[0].message)
+    }
+
+    @Test
+    fun `Given a valid empty description when call new category and validate then should receive ok`() {
+        val expectedName = "Test"
+        val expectedDescription = " "
+        val expectedIsActive = true
+
+        val actualCategory = Category.newCategory(
+            expectedName,
+            expectedDescription,
+            expectedIsActive
+        )
+
+        assertDoesNotThrow { actualCategory.validate(ThrowsValidationHandler()) }
+
+        Assertions.assertNotNull(actualCategory)
+        Assertions.assertNotNull(actualCategory.id)
+        Assertions.assertEquals(expectedName, actualCategory.aName)
+        Assertions.assertEquals(expectedDescription, actualCategory.aDescription)
+        Assertions.assertTrue(actualCategory.isActive)
+        Assertions.assertNotNull(actualCategory.aCreatedAt)
+        Assertions.assertNotNull(actualCategory.aUpdatedAt)
+        Assertions.assertNull(actualCategory.aDeletedAt)
+    }
+
+    @Test
+    fun `Given a valid false isActive when call new category and validate then should receive ok`() {
+        val expectedName = "Test"
+        val expectedDescription = "Test"
+        val expectedIsActive = false
+
+        val actualCategory = Category.newCategory(
+            expectedName,
+            expectedDescription,
+            expectedIsActive
+        )
+
+        assertDoesNotThrow { actualCategory.validate(ThrowsValidationHandler()) }
+
+        Assertions.assertNotNull(actualCategory)
+        Assertions.assertNotNull(actualCategory.id)
+        Assertions.assertEquals(expectedName, actualCategory.aName)
+        Assertions.assertEquals(expectedDescription, actualCategory.aDescription)
+        Assertions.assertFalse(actualCategory.isActive)
+        Assertions.assertNotNull(actualCategory.aCreatedAt)
+        Assertions.assertNotNull(actualCategory.aUpdatedAt)
+        Assertions.assertNull(actualCategory.aDeletedAt)
     }
 }
